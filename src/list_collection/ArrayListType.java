0fa04e18572f7ee5db3a3a9c8a5ccf4ce2ca2ddd
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2023-06-06 08:50:45 +0500

    - ArrayList add function examples

---
+++ src/list_collection/ArrayListType.java
@@ -0,0 +1,35 @@
+package list_collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListType {
+
+    public static void main(String[] args) {
+        addElements();
+    }
+
+    public static void addElements() {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);// adds elements in given order
+        list.add(3);
+        printArrayListElements("output1:",list);
+        
+        list.add(2,6);// adds element at given index(question: can we add element at index 3 instead of 2?)
+        printArrayListElements("output2:",list);
+        
+        list.addAll(List.of(10, 11, 20));// adds elements of given list at end of list
+        printArrayListElements("output3:",list);
+        
+        list.addAll(2, List.of(100,200));// adds elements of given list at specific index
+        printArrayListElements("output4:", list);
+    }
+
+    public static <T> void printArrayListElements(String someString, List<T> list) {
+        System.out.println(someString);
+        for (T t : list) {
+            System.out.println(t);
+        }
+    }
+
+}
